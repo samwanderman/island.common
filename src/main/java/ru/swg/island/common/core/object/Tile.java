934d83104e1bf7fa9e437e46bf5ce05eddd5e6b9
Potapov Sergei <potapov.sa@ueshka.ru>
2016-09-20 17:25:56 +0300

    [+] Shared game files

---
+++ src/main/java/ru/swg/island/common/core/object/Tile.java
@@ -0,0 +1,56 @@
+/**
+ * @author Potapov Sergei (sam-wanderman@yandex.ru)
+ */
+package ru.swg.island.common.core.object;
+
+/**
+ * Class for tiles
+ */
+public final class Tile {
+	// Tile id
+	private String id;
+	// Short tile description
+	private String name;
+	// Path to image
+	private String image;
+	// Tile weight
+	private int weight;
+	// z-index of tile
+	private int z;
+	
+	/**
+	 * Get image path
+	 * 
+	 * @return
+	 */
+	public final String getImage() {
+		return image;
+	}
+	
+	/**
+	 * Get tile name
+	 * 
+	 * @return
+	 */
+	public final String getName() {
+		return name;
+	}
+	
+	/**
+	 * Get tile weight
+	 * 
+	 * @return
+	 */
+	public final int getWeight() {
+		return weight;
+	}
+	
+	/**
+	 * Get z
+	 * 
+	 * @return
+	 */
+	public final int getZ() {
+		return z;
+	}
+}
