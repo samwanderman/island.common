ba23f8848851b7c5213fc147d91f23a32ac173ee
Potapov Sergei <potapov.sa@ueshka.ru>
2016-09-23 16:55:50 +0300

    [+] extended Level object

--- src/main/java/ru/swg/island/common/core/object/Level.java
+++ src/main/java/ru/swg/island/common/core/object/Level.java
@@ -11,6 +11,8 @@ import java.util.List;
 public final class Level {
 	// id
 	private String id;
+	private String name;
+	private String description;
 	// width in cells
 	private int width;
 	// height in cells
@@ -27,6 +29,51 @@ public final class Level {
 	 */
 	public final void setId(final String id) {
 		this.id = id;
+	}
+	
+	/**
+	 * Get id
+	 * 
+	 * @return
+	 */
+	public final String getId() {
+		return id;
+	}
+	
+	/**
+	 * Set name
+	 * 
+	 * @param name
+	 */
+	public final void setName(final String name) {
+		this.name = name;
+	}
+	
+	/**
+	 * Get name
+	 * 
+	 * @return
+	 */
+	public final String getName() {
+		return name;
+	}
+	
+	/**
+	 * Set description
+	 * 
+	 * @param description
+	 */
+	public final void setDescription(final String description) {
+		this.description = description;
+	}
+	
+	/**
+	 * Get description
+	 * 
+	 * @return
+	 */
+	public final String getDescription() {
+		return description;
 	}
 	
 	/**
