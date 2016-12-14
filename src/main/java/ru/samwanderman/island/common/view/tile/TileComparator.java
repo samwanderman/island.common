ceca4a951f52574638a4dbf85e0105864f5aaf85
Potapov SA <sam-wanderman@yandex.ru>
2016-12-14 16:22:31 +0300

    [*] minor improvements

--- src/main/java/ru/samwanderman/island/common/view/tile/TileComparator.java
+++ src/main/java/ru/samwanderman/island/common/view/tile/TileComparator.java
@@ -24,12 +24,15 @@ public final class TileComparator<T extends GuiTile<GuiLevel, ?>> implements Com
 			return -1;
 		}
 
-		// If same layer - tile pos sorting
-		if (((p1.getX() >= p2.getX()) && (p1.getY() > p2.getY())) || ((p1.getX() > p2.getX()) && (p1.getY() >= p2.getY()))) {
+		if (p1.getY() > p2.getY()) {
 			return 1;
+		} else if (p1.getY() < p2.getY()) {
+			return -1;
 		}
 		
-		if (((p1.getX() <= p2.getX()) && (p1.getY() < p2.getY())) || ((p1.getX() < p2.getX()) && (p1.getY() <= p2.getY()))) {
+		if (p1.getX() < p2.getX()) {
+			return 1;
+		} else if (p1.getX() > p2.getX()) {
 			return -1;
 		}
 		
